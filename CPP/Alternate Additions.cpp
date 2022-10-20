64564cbff6500170e4f21b6354fa2ea7a84baca4
priyanshu0056 <61052483+priyanshu0056@users.noreply.github.com>
2022-10-20 19:12:01 +0530

    Added Alternate Additions.cpp
    hacktoberfest 2022

---
+++ CPP/Alternate Additions.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int testc;
+    cin>>testc;
+    while(testc--){
+      
+    long long int pa,pb;
+      
+    cin>>pa>>pb;
+      
+    if((pb-pa)%3==2) cout<<"NO"<<endl;
+      
+    else cout<<"YES"<<endl;
+      
+    }
+  
+return 0;
+}
